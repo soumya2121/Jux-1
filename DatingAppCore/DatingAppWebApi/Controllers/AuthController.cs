2090352576986a3f3dbec152c4f111461c6ec57c
PRIYABRATA BISWAL <priyabrata.biswal@siemens-healthineers.com>
2020-05-30 22:14:59 +0530

    .

--- DatingAppCore/DatingAppWebApi/Controllers/AuthController.cs
+++ DatingAppCore/DatingAppWebApi/Controllers/AuthController.cs
@@ -17,68 +17,68 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace DatingAppWebApi.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Auth")]
-    public class AuthController : Controller
+  [Produces("application/json")]
+  [Route("api/Auth")]
+  public class AuthController : Controller
+  {
+    private IAuthRepository _repo;
+    private IConfiguration _config;
+    private IMapper _mapper;
+
+    public AuthController(IAuthRepository repository, IConfiguration configuration, IMapper mapper)
     {
-        private IAuthRepository _repo;
-        private IConfiguration _config;
-        private IMapper _mapper;
-        
-        public AuthController(IAuthRepository repository,IConfiguration configuration, IMapper mapper)
-        {
-            _repo = repository;
-            _config = configuration;
-            _mapper= mapper;
-        }
-       
-       [HttpPost("register")]
-        public async Task<IActionResult> RegisterUser([FromBody]UserRegistrationDto userRegistrationDto)
-        {
-            
-            if(!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-          
-            if(! await _repo.UserExist(userRegistrationDto.Username.ToLower()))
-            {
-                ModelState.AddModelError("Username", "Username already taken try someother username");
-                return BadRequest(ModelState);
-            }
+      _repo = repository;
+      _config = configuration;
+      _mapper = mapper;
+    }
 
-            var User = _mapper.Map<User>(userRegistrationDto);
-            var CreatedUser= await _repo.RegisterUser(User,userRegistrationDto.Password); 
-            var ReturnUser= _mapper.Map<UserDetailsDto>(User);
-            return CreatedAtRoute("GetUser",new {Controller="user", id=CreatedUser.Id},ReturnUser);
-        }
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterUser([FromBody]UserRegistrationDto userRegistrationDto)
+    {
 
-        [HttpPost("Login")]
-        public async Task<IActionResult> Login([FromBody] UserRegistrationDto userRegistrationDto)
-        {
-            var user = await _repo.LoginUser(userRegistrationDto.Username,userRegistrationDto.Password);
-            if(user==null)
-            {
-                return Unauthorized();
-            }
-            // generate token
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      if (!await _repo.UserExist(userRegistrationDto.Username.ToLower()))
+      {
+        ModelState.AddModelError("Username", "Username already taken try someother username");
+        return BadRequest(ModelState);
+      }
+
+      var User = _mapper.Map<User>(userRegistrationDto);
+      var CreatedUser = await _repo.RegisterUser(User, userRegistrationDto.Password);
+      var ReturnUser = _mapper.Map<UserDetailsDto>(User);
+      return CreatedAtRoute("GetUser", new { Controller = "user", id = CreatedUser.Id }, ReturnUser);
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] UserRegistrationDto userRegistrationDto)
+    {
+      var user = await _repo.LoginUser(userRegistrationDto.Username, userRegistrationDto.Password);
+      if (user == null)
+      {
+        return Unauthorized();
+      }
+      // generate token
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[]
+          {
                     new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                     new Claim(ClaimTypes.Name, user.Username)
-                }),
-                Expires = DateTime.Now.AddDays(1),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha512Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-            var userFromDb = _mapper.Map<UserListDto>(user);
-            return Ok(new {tokenString, userFromDb});
-        }
+          }),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+              SecurityAlgorithms.HmacSha512Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      var tokenString = tokenHandler.WriteToken(token);
+      var userFromDb = _mapper.Map<UserListDto>(user);
+      return Ok(new { tokenString, userFromDb });
     }
+  }
 }
